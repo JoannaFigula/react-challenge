c365864c78231ce0d6cba477bd1dd3103a326a90
Joanna <48253408+JoannaFigula@users.noreply.github.com>
2022-03-26 14:31:37 +0100

    cherry 2

--- client/src/theme.js
+++ client/src/theme.js
@@ -164,15 +164,6 @@ theme = createTheme(theme, {
         },
       },
     },
-    MuiTableRow: {
-      styleOverrides: {
-        root: {
-          '&.Mui-selected': {
-            backgroundColor: '#F9FAFD',
-          },
-        },
-      },
-    },
     MuiButton: {
       defaultProps: {
         variant: 'outlined',
